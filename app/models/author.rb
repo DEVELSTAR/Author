5d6c7a209eb82d235b037b2caf924281d849f4c3
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-08-25 02:31:08 +0530

    add flash messages

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,16 @@
 class Author < ApplicationRecord
   has_many :joins
   has_many :books, through: :joins, dependent: :destroy
+
+   validates :first_name, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 25 }
+    validates :last_name, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 25 }
+
+    validates :date_of_birth, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 25 }
+
 end
